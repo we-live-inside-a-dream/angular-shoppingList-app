8d7d5642646c67b0d957a135b11802219905bcc3
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-07 20:52:57 -0600

    created a recipe in the list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2022/05/11/dining/06Apperex-polenta/merlin_204759390_6b518d19-ce2c-462e-bad4-d17d5d63d404-articleLarge.jpg'),
+  ];
 
   constructor() { }
 
