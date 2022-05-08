e14f86262917e9e1064c554542b12381d93d48cd
Reza Naeim-Abadi <rezanaeimabadi7@gmail.com>
2022-05-07 20:55:02 -0600

    added new recipe

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2022/05/11/dining/06Apperex-polenta/merlin_204759390_6b518d19-ce2c-462e-bad4-d17d5d63d404-articleLarge.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static01.nyt.com/images/2022/05/11/dining/06Apperex-polenta/merlin_204759390_6b518d19-ce2c-462e-bad4-d17d5d63d404-articleLarge.jpg'),
   ];
 
   constructor() { }
